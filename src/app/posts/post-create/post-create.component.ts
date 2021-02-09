e725ff2b84f1483beef75d725c3dc39b7ad5b889
Abirame-29 <nalinidhevan@gmail.com>
2021-02-09 20:34:50 +0530

    Post list added as a new component to view the list of post created as Expansion panel. Toolbar is also added to the app.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +9,15 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = '';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post: Post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
