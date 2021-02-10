336b27e788f4e25704ccf7a1f8e3a6fb715cd660
Abirame-29 <nalinidhevan@gmail.com>
2021-02-10 13:10:56 +0530

    Adding observable to the form. Edit and Delete button added to expandable panel. Logic for the buttons is to be added.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   {title: 'First Post', content: 'This is first post\'s content'},
   //   {title: 'Second Post', content: 'This is second post\'s content'},
   //   {title: 'Third Post', content: 'This is third post\'s content'}
   // ]
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postSub: Subscription;
 
+  constructor (public postsSevice: PostsService) {}
 
+  ngOnInit() {
+    this.posts = this.postsSevice.getPosts();
+    this.postSub = this.postsSevice.getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
 }
