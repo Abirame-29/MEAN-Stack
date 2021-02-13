62124b035bc6aab5a93133096dfe615e80006466
Abirame-29 <nalinidhevan@gmail.com>
2021-02-13 16:02:30 +0530

    Connected to MongoDB database and save post inside the database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://abi:ktsWHwpKvQDx16YW@cluster0.fjzid.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -11,10 +22,15 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully!'
   })
