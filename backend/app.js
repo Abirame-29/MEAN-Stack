abb53a4f3ce15049d2b3f07370d03a2d67552d38
Abirame-29 <nalinidhevan@gmail.com>
2021-02-14 21:06:39 +0530

    Toolbar added for creating and editing post

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.delete('/api/posts/:id',(req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then((result) => {
     console.log(result);
     res.status(200).json({message: 'Post deleted'});
-  })
+  });
 
 });
 
