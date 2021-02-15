3cdfadb5bfa5b06b4da45c269509f3efc22c194f
Abirame-29 <nalinidhevan@gmail.com>
2021-02-15 22:50:02 +0530

    Edit post added to the application

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -33,12 +33,28 @@ export class PostsService {
     return this.postUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         post.id = responseData.postId;
         this.posts.push(post);
+        this.postUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string){
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
         this.postUpdated.next([...this.posts]);
       });
   }
